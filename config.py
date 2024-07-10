a18284aac6a52829c30973d4bb9b4cd717252929
122344AS <143315452+122344AS@users.noreply.github.com>
2024-07-10 14:41:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -101,9 +101,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "changing-bettye-khurshid-5ee3d7ad.koyeb.app/"
     else:
-        URL = ""
+        URL = "changing-bettye-khurshid-5ee3d7ad.koyeb.app/"
 
 
 
